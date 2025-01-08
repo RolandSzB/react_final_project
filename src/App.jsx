412f008b6bb9eccfb3394c6328ff7b93632e35f0
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-01-08 18:02:31 +0200

    tailwindcss installed for page design

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import "./App.css";
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
