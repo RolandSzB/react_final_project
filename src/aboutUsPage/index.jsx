1fd769197250b841894df0e728f85b4103eacf3a
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-01-09 11:53:04 +0200

    about us page made

--- src/aboutUsPage/index.jsx
+++ src/aboutUsPage/index.jsx
@@ -1,35 +1,55 @@
 import { Link } from "react-router-dom";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css";
+import aboutImage from "./about.png";
 
 export default function AboutUsPage() {
   return (
-    <div className="flex flex-col">
-      <div>
-        <h2 className="underline">About Us</h2>
-        <h2 className="text-yellow-400">Your Best Choice</h2>
-        <p>
-          MyBestChoice is your one-stop online store for top-rated home
-          appliances and electronics. We offer a handpicked selection of the
-          best refrigerators, tablets, laptops, and more to meet all your
-          household and tech needs. Discover exceptional quality, unbeatable
-          prices, and reliable customer service for every purchase—only at
-          BestChoice!
-        </p>
-        <i className="bi bi-person-check-fill ">
-          We offer a handpicked selection of the best tablets, laptops, and more
-          to meet all your household and tech needs. The selection is made by
-          real humans, without any use of AI
-        </i>
+    <div className="flex flex-col bg-teal-100">
+      <h2 className="flex content-center justify-center font-bold text-5xl m-8">
+        About Us
+      </h2>
+      <p className="text-lg text-gray-500 text-center m-12">
+        More than only a price comparition site! We are here to help you in
+        picking the best tech form your remote work, gaming or other activities.
+        We want to help you, to be there for you, not only with the lowest price
+        but also with the best specs.
+      </p>
+      <div className="flex flex-row justify-center">
+        <div className=" flex flex-col ms-12 mt-4 p-8">
+          <div className="flex flex-row mb-4">
+            <i className="bi bi-laptop text-4xl font-bold me-6 "></i>
+            <p className="text-lg content-center">
+              We offer a handpicked selection of the best tablets, laptops, and
+              more to meet all your household and tech needs.
+            </p>
+          </div>
+          <div className="flex flex-row mb-4">
+            <i className="bi bi-person-check-fill text-4xl font-bold me-6"></i>
+            <p className="text-lg content-center">
+              The selection is made by real humans, without any use of AI
+            </p>
+          </div>
+          <div className="flex flex-row">
+            <i className="bi bi-stars text-4xl font-bold me-6"></i>{" "}
+            <p className="text-lg content-center">
+              {" "}
+              10 years of experience in the tech industry
+            </p>
+          </div>
+          <button
+            type="button"
+            className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200  font-medium rounded-lg text-xl px-5 py-4 text-center mt-40 mx-64"
+          >
+            <i className="bi bi-house me-2"></i>
+            <Link to="/">Home</Link>
+          </button>
+        </div>
+        <div>
+          <img src={aboutImage} alt="" className=" h-3/4 rounded-md" />
+        </div>
       </div>
-      <div>
-        <button
-          type="button"
-          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
-        >
-          <Link to="/">Home</Link>
-        </button>
-      </div>
+      <div></div>
     </div>
   );
 }
