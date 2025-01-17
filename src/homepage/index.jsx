31bd24fad55af6b43eda32752c205ae6a70df910
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-01-17 11:45:00 +0200

    best items added

--- src/homepage/index.jsx
+++ src/homepage/index.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Welcome from "./welcome";
 import BestItems from "./items";
 
 const HomePage = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    // Simulate fetching data from an API or JSON file
+    const jsonData = {
+      items: [
+        {
+          name: "Laptop Apple MacBook Pro 14",
+          image:
+            "https://s13emagst.akamaized.net/products/80627/80626261/images/res_6e48dac5ebdc3ffda7e893d02c20eda0.jpg",
+          price: 2399.0,
+          rating: 4.7,
+        },
+        {
+          name: "Asus ROG Zephyrus G14",
+          image:
+            "https://www.pcworld.com/wp-content/uploads/2024/01/ASUS_ROG_Zephyrus_G14_GA402_1.jpg",
+          price: 2500.0,
+          rating: 4.9,
+        },
+        {
+          name: "LENOVO Yoga Pro 9",
+          image:
+            "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6571/6571565_sd.jpg",
+          price: 2523.86,
+          rating: 4.7,
+        },
+      ],
+    };
+
+    setItems(jsonData.items);
+  }, []);
+
   return (
     <div>
       <Welcome />
-      <BestItems />
+      <div className="flex flex-wrap gap-4">
+        {items.map((item, index) => (
+          <BestItems
+            key={index}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+            rating={item.rating}
+          />
+        ))}
+      </div>
     </div>
   );
 };
+
 export default HomePage;
--- src/homepage/items.jsx
+++ src/homepage/items.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "./index.css";
 
-const BestItems = () => {
+const BestItems = ({ name, image, price, rating }) => {
   return (
-    <div>
-      <h1>Best Item 1</h1>
-      <h1>Best Item 2</h1>
-      <h1>Best Item 3</h1>
+    <div className="flex flex-col content-center justify-center text-center bg-white">
+      <img src={image} alt={name} className="w-20 h-20" />
+      <h1>{name}</h1>
+      <h2>${price.toFixed(2)}</h2>
+      <h3>{rating} ⭐</h3>
     </div>
   );
 };
+
+BestItems.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
 export default BestItems;
