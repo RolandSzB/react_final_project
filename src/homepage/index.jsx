97c23e3bab5785a6322ec055ca8ddaae8c9b3541
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-01-17 12:19:32 +0200

    best items styled

--- src/homepage/index.jsx
+++ src/homepage/index.jsx
@@ -10,25 +10,28 @@ const HomePage = () => {
     const jsonData = {
       items: [
         {
-          name: "Laptop Apple MacBook Pro 14",
+          name: "Apple MacBook Pro 14",
           image:
             "https://s13emagst.akamaized.net/products/80627/80626261/images/res_6e48dac5ebdc3ffda7e893d02c20eda0.jpg",
           price: 2399.0,
           rating: 4.7,
+          link: "https://www.emag.ro/laptop-apple-macbook-pro-14-cu-procesor-apple-m4-pro-12-nuclee-cpu-si-16-nuclee-gpu-24gb-ram-512gb-ssd-space-black-tastatura-internationala-manual-ro-mx2h3ro-a/pd/DZNPNRYBM/?ref=sponsored_products_listing_r_1_1&recid=recads_1_42385e1276997bc5cd591df5129f46c18d9344e9353f99e9eec0e05152997da4_1737109000&aid=a968c36a-c11d-11ef-8003-06eb3cbbbd1b&oid=208736395&aidr=a968b998-c11d-11ef-8003-06eb3cbbbd1b&scenario_ID=1",
         },
         {
           name: "Asus ROG Zephyrus G14",
           image:
-            "https://www.pcworld.com/wp-content/uploads/2024/01/ASUS_ROG_Zephyrus_G14_GA402_1.jpg",
+            "https://dlcdnwebimgs.asus.com/gain/47215993-C79C-4761-9F1C-EBFA5D2E029B",
           price: 2500.0,
           rating: 4.9,
+          link: "https://www.emag.ro/laptop-gaming-asus-rog-zephyrus-g14-ga403ui-cu-procesor-amd-ryzentm-9-8945hs-pana-la-5-2-ghz-14-3k-oled-120hz-32gb-ddr5-1tb-ssd-nvidiar-geforce-rtxtm-4070-8gb-gddr6-no-os-eclipse-gray-ga403ui-qs051/pd/DGNP8TYBM/",
         },
         {
           name: "LENOVO Yoga Pro 9",
           image:
-            "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/6571/6571565_sd.jpg",
+            "https://p4-ofp.static.pub//fes/cms/2024/11/01/xuckml3h1inmw4pscngfegm9i0y7zn840754.png",
           price: 2523.86,
           rating: 4.7,
+          link: "https://www.lenovo.com/ro/ro/p/laptops/yoga/yoga-pro-series/yoga-pro-9i-gen-8-%2814-inch-intel%29/len101y0036?orgRef=https%253A%252F%252Fwww.google.com%252F",
         },
       ],
     };
@@ -39,16 +42,20 @@ const HomePage = () => {
   return (
     <div>
       <Welcome />
-      <div className="flex flex-wrap gap-4">
-        {items.map((item, index) => (
-          <BestItems
-            key={index}
-            name={item.name}
-            image={item.image}
-            price={item.price}
-            rating={item.rating}
-          />
-        ))}
+      <div className="flex flex-col content-center items-center ">
+        <h1 className="font-bold text-3xl hover:underline mt-12">Best Items</h1>
+        <div className="flex flex-wrap gap-4 justify-between mx-36 mb-8">
+          {items.map((item, index) => (
+            <BestItems
+              key={index}
+              name={item.name}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+              link={item.link}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/homepage/items.jsx
+++ src/homepage/items.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./index.css";
 
-const BestItems = ({ name, image, price, rating }) => {
+const BestItems = ({ name, image, price, rating, link }) => {
   return (
-    <div className="flex flex-col content-center justify-center text-center bg-white">
-      <img src={image} alt={name} className="w-20 h-20" />
-      <h1>{name}</h1>
-      <h2>${price.toFixed(2)}</h2>
+    <div className="flex flex-col content-center items-center justify-center text-center bg-white px-8 pb-8">
+      <img src={image} alt={name} className="w-72 h-72" />
+      <h1 className="font-bold text-xl hover:underline">{name}</h1>
+      <h2 className="text-xl hover:text-red-800">${price.toFixed(2)}</h2>
       <h3>{rating} ⭐</h3>
+      <a
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="bg-lime-600 rounded-lg text-white text-lg px-4 py-2 hover:bg-lime-500 hover:text-xl hover:font-bold inline-block"
+      >
+        Buy Now! <i className="bi bi-arrow-right"></i>
+      </a>
     </div>
   );
 };
@@ -18,6 +26,7 @@ BestItems.propTypes = {
   image: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
   rating: PropTypes.number.isRequired,
+  link: PropTypes.string.isRequired,
 };
 
 export default BestItems;
