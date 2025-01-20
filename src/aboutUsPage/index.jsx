3607fa2a051b718bc41c2220d755762b2c3d8802
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-01-20 11:20:38 +0200

    jump icon added to whole button

--- src/aboutUsPage/index.jsx
+++ src/aboutUsPage/index.jsx
@@ -38,9 +38,9 @@ export default function AboutUsPage() {
           </div>
           <button
             type="button"
-            className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200  font-medium rounded-lg text-xl px-5 py-4 text-center mt-40 mx-64"
+            className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200  font-medium rounded-lg text-xl px-5 py-4 text-center mt-40 mx-64 icon-jump"
           >
-            <i className="bi bi-house me-2 icon-jump"></i>
+            <i className="bi bi-house me-2 "></i>
             <Link to="/">Home</Link>
           </button>
         </div>
