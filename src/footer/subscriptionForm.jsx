16fb3f63c29a5de43f256766d725a7c2dc5a786b
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-01-20 11:08:51 +0200

    when form submitted, submitting message given

--- src/footer/subscriptionForm.jsx
+++ src/footer/subscriptionForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field, useFormikContext } from "formik";
 import * as Yup from "yup";
 
@@ -8,6 +8,8 @@ const ErrorMsg = ({ name }) => {
 };
 
 function SubscriptionForm() {
+  const [successMessage, setSuccessMessage] = useState("");
+
   const handleSubmit = async (values, resetForm) => {
     try {
       const response = await fetch(
@@ -20,11 +22,17 @@ function SubscriptionForm() {
           body: JSON.stringify(values),
         }
       );
-      const result = await response.json();
-      resetForm();
-      console.log("Form submitted successfully:", result);
+      if (response.ok) {
+        const result = await response.json();
+        setSuccessMessage("Form submitted successfully!");
+        resetForm();
+        console.log("Form submitted successfully:", result);
+      } else {
+        setSuccessMessage("Error submitting form.");
+      }
     } catch (error) {
       console.error("Error submitting form:", error);
+      setSuccessMessage("Error submitting form.");
     }
   };
 
@@ -32,20 +40,18 @@ function SubscriptionForm() {
     email: "",
     interest: "",
   };
+
   const validationSchema = Yup.object({
     email: Yup.string().required("Email is required").email("Invalid email"),
     interest: Yup.string().required("Interest is required"),
   });
-  const handleReset = (resetForm) => {
-    resetForm();
-  };
 
   return (
     <div className="flex flex-col content-center px-96">
-      <div className=" flex flex-col text-center font-semibold text-3xl my-8">
+      <div className="flex flex-col text-center font-semibold text-3xl my-8">
         Subscribe
       </div>
-      <div className=" flex flex-col text-center text-lg my-2">
+      <div className="flex flex-col text-center text-lg my-2">
         Subscribe to our newsletter and be among the first to hear about new
         arrivals, events and special offers.
       </div>
@@ -56,13 +62,13 @@ function SubscriptionForm() {
       >
         {({ resetForm }) => (
           <Form>
-            <label className="flex flex-col  px-40 ">
+            <label className="flex flex-col px-40">
               Email:
               <Field
                 type="text"
                 name="email"
                 placeholder="Enter your email"
-                className="bg-transparent border-2	border-black rounded-3xl p-3 placeholder-black hover:bg-green-400 "
+                className="bg-transparent border-2 border-black rounded-3xl p-3 placeholder-black hover:bg-green-400"
               />
               <ErrorMsg name={"email"} />
             </label>
@@ -75,7 +81,7 @@ function SubscriptionForm() {
                 <Field
                   as="select"
                   name="interest"
-                  className="bg-transparent border-2	border-black rounded-3xl p-3 placeholder-black hover:bg-green-400"
+                  className="bg-transparent border-2 border-black rounded-3xl p-3 placeholder-black hover:bg-green-400"
                 >
                   <option value="">Select</option>
                   <option value="laptop">Laptop</option>
@@ -86,8 +92,7 @@ function SubscriptionForm() {
 
               <button
                 type="submit"
-                className="bg-black text-white ms-8 my-8 px-6 py-2 rounded-xl border-4 border-black  hover:bg-gray-700 "
-                onClick={() => handleReset(resetForm)}
+                className="bg-black text-white ms-8 my-8 px-6 py-2 rounded-xl border-4 border-black hover:bg-gray-700"
               >
                 Submit
               </button>
@@ -97,7 +102,14 @@ function SubscriptionForm() {
           </Form>
         )}
       </Formik>
+
+      {successMessage && (
+        <div className="text-center text-red-800 font-bold text-2xl">
+          {successMessage}
+        </div>
+      )}
     </div>
   );
 }
+
 export default SubscriptionForm;
