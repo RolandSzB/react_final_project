4bfc444e720fb8d35751b7968a5c9d0ac932fe2c
SziBe Roland <roland.szilagyi-benedek@emanuel.ro>
2025-01-20 10:43:25 +0200

    home link added to icon

--- src/navbar/index.jsx
+++ src/navbar/index.jsx
@@ -5,7 +5,10 @@ import logoImage from "./logo.webp";
 export default function NavBar() {
   return (
     <div className="flex items-center justify-between p-4 bg-gray-200">
-      <img src={logoImage} alt="Logo" className="w-16 h-16" />
+      <Link to="/">
+        <img src={logoImage} alt="Logo" className="w-16 h-16" />
+      </Link>
+
       <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-8">
         <Link to="/laptops" className="hover:text-blue-500 font-medium">
           Laptops
